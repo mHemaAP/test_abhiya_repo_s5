2a8c875c257d53d131990db3ab3dfac2b041f53c
abhiyagupta <90094648+abhiyagupta@users.noreply.github.com>
2024-10-01 00:14:09 +0530

    Update eval.py

--- 3_assignment_pytorch_lightening/utils/eval.py
+++ 3_assignment_pytorch_lightening/utils/eval.py
@@ -6,8 +6,19 @@ def main():
     # Load data module
     data_module = DogsDataModule(dl_path='./data', batch_size=32)
     
+    # Explicitly call prepare_data and setup
+    data_module.prepare_data()
+    data_module.setup()
+
+    # Get the number of classes
+    num_classes = data_module.get_num_classes()
+    print(f"Number of classes in the dataset: {num_classes}")
+
     # Load the best model using the last checkpoint
-    model = DogsClassifier.load_from_checkpoint("checkpoints/dogs_classifier-best_val_loss.ckpt")
+    model = DogsClassifier.load_from_checkpoint(
+        "checkpoints/dogs_classifier-best_val_loss.ckpt",
+        num_classes=num_classes  # Pass the correct number of classes
+    )
     
     # Create trainer
     trainer = pl.Trainer(accelerator='auto')
